5ba0e26946b5392f99f4e7bae5722398ec52e9dc
Charlie Andrews <charlieandrews.cwa@gmail.com>
2015-12-08 21:49:09 -0600

    Set up main loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let prompt = ":";
+    loop {
+        print!("{}", prompt);
+        io::stdout().flush().unwrap();
+        let mut line = String::new();
+        match io::stdin().read_line(&mut line) {
+            Ok(_) => (),
+            Err(_) => {
+                println!("?");
+                continue
+            }
+        }
+        println!("You typed in {}", line.trim());
+    }
 }
