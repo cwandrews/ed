c977dce2bb342d5c9ffa30487a559e985444cb3a
Charlie Andrews <charlieandrews.cwa@gmail.com>
2015-12-09 20:35:33 -0600

    Allocate a new line for everytime through the loop

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
 use std::io;
-use std::io::Write;
+
+enum Mode {
+    Command,
+    Input,
+}
+
+
+fn command_line(line: &String, mode: &mut Mode) {
+    match line.trim() {
+        "a" => *mode = Mode::Input,
+        "q" => std::process::exit(0),
+        _ => println!("Command: {}", line),
+    }
+}
+
+fn input_line(line: &String, mode: &mut Mode) {
+    match line.trim() {
+        "." => *mode = Mode::Command,
+        _ => println!("Input: {}", line),
+    }
+}
 
 fn main() {
-    let prompt = ":";
+    let mut mode = Mode::Command;
     loop {
-        print!("{}", prompt);
-        io::stdout().flush().unwrap();
         let mut line = String::new();
         match io::stdin().read_line(&mut line) {
             Ok(_) => (),
             Err(_) => {
                 println!("?");
                 continue
-            }
+            },
         }
-        println!("You typed in {}", line.trim());
+        match mode {
+            Mode::Command => command_line(&line, &mut mode),
+            Mode::Input => input_line(&line, &mut mode),
+        }
     }
 }
